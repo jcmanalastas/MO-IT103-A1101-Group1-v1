f1a95d9a771c943b31673c5b0ce18870e42f9dad
SeanQuiben <lr.sgquiben@mmdc.mcl.edu.ph>
2025-05-19 00:16:52 +0800

    commit init

--- src/MotorPH.java
+++ src/MotorPH.java
@@ -4,18 +4,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.*;
+import java.util.regex.*;
 
 public class MotorPH {
     public static void main(String[] args) {
         String path = "Data.csv";
         List<String[]> data = new ArrayList<>();
+        String csvRegex = "\"([^\"]*)\"|([^,]+)";
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                data.add(values);
+                List<String> values = new ArrayList<>();
+                Matcher matcher = Pattern.compile(csvRegex).matcher(line);
+
+                while (matcher.find()){
+                    values.add(matcher.group(1) !=null ? matcher.group(1) : matcher.group(2));
+                }
+
+                data.add(values.toArray(new String[0]));
             }
             br.close();
         } catch (FileNotFoundException e) {
@@ -33,13 +41,14 @@ public class MotorPH {
         scanner.close();
 
         // Search for Employee ID
-        boolean found = false;
 
-        System.out.printf("%-15s %-20s %-25s\n", "Employee Number", "Employee Name", "Employee Address");
+
+        boolean found = false;
 
         for (String[] row : data) { // Single loop to find matching Employee ID
             if (row.length > 0 && row[0].equals(inputID)) {
                 // Print only specific columns
+                System.out.printf("%-15s %-20s %-25s\n", "Employee Number", "Employee Name", "Employee Address");
                 System.out.printf("%-15s %-20s %-25s\n",
                         row[0].replace("\"", ""), // Employee Number
                         row[1].replace("\"", "") + " " + row[2].replace("\"", ""), // Employee Name
