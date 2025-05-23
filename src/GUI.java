42cb297cbdf84a740d0be3f39bb3c072e1f16a67
Group1 <lr.sgquiben@mmdc.mcl.edu.ph>
2025-05-24 06:28:55 +0800

    Commit init - added GUI for MotorPH

---
+++ src/GUI.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class GUI extends JFrame {
+    private JPanel main;
+    private JTextField txtEmpNum;
+    private JComboBox cmbPayPeriod;
+    private JRadioButton btn15;
+    private JRadioButton btn30;
+    private JTable txtPayslip;
+    private JButton btnSearch;
+
+    public GUI() {
+        setTitle("MotorPH Payroll System");
+        setContentPane(main);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 400);
+        setLocationRelativeTo(null);
+        createTable();
+    }
+
+    private void createTable() {
+        txtPayslip.setModel(new DefaultTableModel(
+                null,
+                new String[]{"Pay Start", "Pay End", "Total Hours", "Gross Salary", "SSS", "PhilHealth", "Pag-IBIG", "Tax", "Total Deductions", "Net Pay"}
+        ));
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GUI().setVisible(true));
+    }
+}
